65f36386dcab8d75b6c6c0689a3f304aa3ab2114
Dbaldwin2025 <dbaldwin2025@gmail.com>
2024-05-18 11:50:40 -0600

    update

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -14,3 +14,17 @@ WHERE account_id = 1;
 UPDATE public.inventory 
 SET inv_description = REPLACE(inv_description , 'small interiors', 'a huge interior') 
 WHERE inv_model = 'Hummer';
+
+
+
+
+SELECT inv_make, inv_model FROM public.inventory
+INNER JOIN public.classification
+ON inventory.classification_id = classification.classification_id
+WHERE classification_name = 'Sport';
+
+
+
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image , 'images', 'images/vehicles'),
+inv_thumbnail = REPLACE(inv_thumbnail , 'images', 'images/vehicles');
