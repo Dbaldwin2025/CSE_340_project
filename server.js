fab0efcc82987ad7b64f738cb978b6331f03fa03
Dbaldwin2025 <dbaldwin2025@gmail.com>
2024-05-25 19:18:51 -0600

    updates

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
+
+
 /* ***********************
  * Routes
  *************************/
@@ -30,13 +32,18 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use("/inv", inventoryRoute)
  
 app.use(static)
+
 /*
 app.get("/", function(req, res) {
   res.render("index", {title: "Home"})
 })
 */
 
-app.get("/", baseController.buildHome) 
+//app.get("/", baseController.buildHome) 
+
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
@@ -47,12 +54,25 @@ app.use(async (req, res, next) => {
 * Express Error Handler
 * Place after all other middleware
 *************************/
+/*
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message: err.message,
+    nav
+  })
+})
+*/
+
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! The app crashed. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
     nav
   })
 })
